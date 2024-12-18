b26b1737f57c6f60c935ac641472ce96544c8f64
papaB3A <papa.akumu@student.moringaschool.com>
2024-12-18 09:53:35 +0300

    Completed Moringa-FT09-phase-3-code-challenge

--- models/author.py
+++ models/author.py
@@ -1,7 +1,148 @@
+from database.connection import get_db_connection
+
 class Author:
+    all = {}
+
     def __init__(self, id, name):
         self.id = id
         self.name = name
 
     def __repr__(self):
-        return f'<Author {self.name}>'
+        return f'<Author {self.id} {self.name}>'
+    
+
+    @property
+    def id(self):
+        return self._id
+    
+    @id.setter
+    def id(self, id):
+        if isinstance(id, int):
+            self._id = id
+
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        if hasattr(self, '_name'):
+            raise AttributeError("Name cannot be changed after initializing")
+        else:
+            if isinstance(new_name, str):
+                if len(new_name) > 0:
+                    self._name = new_name
+
+
+    def save(self):
+        conn = get_db_connection()
+        CURSOR = conn.cursor()
+        sql = """
+            INSERT INTO authors (name)
+            VALUES (?)
+        """
+        CURSOR.execute(sql, (self.name,))
+        conn.commit()
+        
+        self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self
+
+    @classmethod
+    def create(cls, name):
+        author = cls(name)
+        author.save()
+        return author
+    
+    def get_author_id(self):
+        return self.id
+
+    def articles(self):
+        from models.article import Article
+        conn = get_db_connection()
+        CURSOR = conn.cursor()
+        """retrieves and returns a list of articles wriitten by this author"""
+        sql = """
+            SELECT ar.*
+            FROM articles ar
+            INNER JOIN authors a ON ar.author = a.id
+            WHERE a.id = ?
+        """
+
+        CURSOR.execute(sql, (self.id,))
+        article_data = CURSOR.fetchall()
+
+        articles = []
+        for row in article_data:
+            articles.append(Article(*row))
+        return articles
+    
+
+    def magazines(self):
+        from models.magazine import Magazine
+        conn = get_db_connection()
+        CURSOR = conn.cursor()
+        """Retrieves and returns a list of Magazine objects where this Author has written articles"""
+        sql = """
+            SELECT DISTINCT m.*
+            FROM magazines m
+            INNER JOIN articles ar ON ar.magazine = m.id
+            INNER JOIN authors a ON ar.author = a.id
+            WHERE a.id = ?
+        """
+
+        CURSOR.execute(sql, (self.id,))
+        magazine_data = CURSOR.fetchall()
+
+        magazines = []
+        for row in magazine_data:
+            magazines.append(Magazine(*row))
+        return magazines
+    
+    def update(self):
+        conn = get_db_connection()
+        """Update the table row corresponding to the current Article instance."""
+        sql = """
+            UPDATE authors
+            SET author_id = ?, name = ?
+            WHERE id =?
+        """
+        cur = conn.cursor()
+        cur.execute(sql, (self.author_id, self.name))
+        conn.commit()
+
+    def delete(self):
+        conn = get_db_connection()
+        """Delete the table row corresponding to the current Article instance,
+        delete the dictionary entry, and reassign id attribute"""
+
+        sql = """
+            DELETE FROM authors
+            WHERE id =?
+        """
+
+        cur = conn.cursor()
+        cur.execute(sql, (self.author_id,))
+        conn.commit()
+
+        del type(self).all[self.author_id]
+
+        self.id = None
+
+    @classmethod
+    def find_by_id(cls, author_id):
+        """Find an author by id"""
+        conn = get_db_connection()
+        CURSOR = conn.cursor()
+        sql = """
+            SELECT *
+            FROM authors
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (author_id,))
+        author_data = CURSOR.fetchone()
+
+        if author_data:
+            return cls(*author_data)
+        else:
+            return None
--- models/magazine.py
+++ models/magazine.py
@@ -1,8 +1,97 @@
+from database.connection import get_db_connection
+
 class Magazine:
     def __init__(self, id, name, category):
-        self.id = id
-        self.name = name
-        self.category = category
+        self._id = id
+        self._name = name
+        self._category = category
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, id):
+        if not isinstance(id, int):
+            raise TypeError("id must be of type int")
+        self._id = id
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 2 <= len(name) <= 16:
+            self._name = name
+        else:
+            raise TypeError("name must be of type str and between 2 and 16 characters")
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, category):
+        if isinstance(category, str) and len(category) > 0:
+            self._category = category
+        else:
+            raise TypeError("category must be of type str and longer than zero characters")
+
+    @property
+    def articles(self):
+        """
+        Property method to retrieve the articles of the magazine.
+        """
+        from models.article import Article
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('''
+            SELECT articles.id, articles.title, articles.content, articles.author_id, articles.magazine_id
+            FROM articles 
+            WHERE articles.magazine_id = ?
+        ''', (self.id,))
+        article_info = cursor.fetchall()
+        conn.close()
+        if article_info:
+            return [Article(article["id"], article['title'], article['content'], article['author_id'], article['magazine_id']) for article in article_info]
+        else:
+            return []
+
+    @property
+    def contributors(self):
+        """
+        Property method to retrieve the contributors of the magazine.
+        """
+        from models.author import Author
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        sql = '''
+            SELECT DISTINCT authors.id, authors.name
+            FROM authors 
+            JOIN articles  ON authors.id = articles.author_id
+            WHERE articles.magazine_id = ?
+        '''
+        cursor.execute(sql, (self.id,))
+        author_info = cursor.fetchall()
+        conn.close()
+        if author_info:
+            return [Author(author["id"], author['name']) for author in author_info]
+        else:
+            return []
+
+    def article_titles(self):
+        return [article.title for article in self.articles]
+
+    def contributing_authors(self):
+        if self.contributors:
+            contributing = [contributor for contributor in self.contributors if len([article for article in self.articles if article.author.id == contributor.id]) > 2]
+            return contributing
+        else:
+            return None
 
     def __repr__(self):
-        return f'<Magazine {self.name}>'
+        contributer_titles = "; ".join([contributer.name for contributer in self.contributors]) if self.contributors else "None"
+        major_contributors = ";".join([contributer.name for contributer in self.contributing_authors()]) if self.contributing_authors() else "None"
+        article_titles = ";".join([article.title for article in self.articles]) if self.articles else "None"
+        return f'MAGAZINE:{self.name} ||ID: {self.id} || ARTIC:{article_titles}|| CONTR:{contributer_titles} || MAJOR CONTR:{major_contributors}'
